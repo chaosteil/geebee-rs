66f7aaf490c993a85390d1df0dd0e8cabc1fb999
Dominykas Djacenko <chaosteil@gmail.com>
2020-05-10 10:33:59 -0700

    Scale out window

--- src/ui.rs
+++ src/ui.rs
@@ -3,8 +3,10 @@ use crate::cpu::CPU;
 use ::image as im;
 use piston_window::*;
 
+const SCALE: u32 = 2;
+
 pub fn launch(mut cpu: CPU) -> Result<(), Box<dyn std::error::Error>> {
-    let mut window: PistonWindow = WindowSettings::new("GeeBee", (160, 144))
+    let mut window: PistonWindow = WindowSettings::new("GeeBee", (160 * SCALE, 144 * SCALE))
         .resizable(false)
         .build()?;
     window.set_ups(60);
@@ -17,7 +19,7 @@ pub fn launch(mut cpu: CPU) -> Result<(), Box<dyn std::error::Error>> {
         cpu.lcd().screen(),
         160,
         144,
-        &TextureSettings::new(),
+        &TextureSettings::new().filter(texture::Filter::Nearest),
     )
     .unwrap();
     while let Some(e) = window.next() {
@@ -33,7 +35,7 @@ pub fn launch(mut cpu: CPU) -> Result<(), Box<dyn std::error::Error>> {
                     &im::RgbaImage::from_vec(160, 144, screen.to_vec()).unwrap(),
                 )
                 .unwrap();
-            image(&texture, c.transform, g);
+            image(&texture, c.transform.zoom(SCALE as f64), g);
         });
     }
     Ok(())
